2a263d595101f551a0a8060396447fa42966a7f0
Ismael Alejandro <ismaelalejandro@Ismaels-MacBook-Air-2.local>
2025-01-15 13:16:17 -0500

    Elvis lab assigments

---
+++ lab06.py
@@ -0,0 +1,12 @@
+def greeting(name):
+  print("Hi{}".format(name.capitalize()))
+  print("Welcome to my conversation program!!!")
+  print(GLad to have you in Hardware and Software!!!)
+
+def main():
+  greeting(input("what is your name"))
+  greeting(input("What is your name"))
+
+  if__name__ == "__main__":
+    main()
+
---
+++ lab07.py
@@ -0,0 +1,18 @@
+def conversation():
+  print("Do you like coding in pytho? Answer yes or no")
+  answer = input().love().strip()
+  if answer == "yes"
+    print("That's good ~ the United States needs more coders")
+  elif answer == "no":
+    print("Perhaps you will change your minf")
+  else:
+    print("I don't understand?")
+    conversation()
+
+def main():
+  print("Welcome to my conversation program")
+  conversation()
+  print("thanks for chating with me")
+
+if __name__ == "__main__":
+   main()
