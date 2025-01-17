e3e071078dd12ebf0a767d2f3f839aab69743d3b
Ismael Alejandro <ismaelalejandro@Ismaels-MacBook-Air-2.local>
2025-01-17 12:35:37 -0500

    working in project01

---
+++ ea_project01.py
@@ -0,0 +1,17 @@
+def interview():
+   print("Hellow, I would like to get to know allitle about you, and I want to know how much you know about soccer.")
+   print("Please answer a few brief questions about soccer")
+
+   name = input("Wat is your name?")
+   ege = input("how old are you?")
+   soccer = input("Do you like Futbol? Answer Yes or not")
+   answer = input(yes)
+   if answer == "yes":
+      print("I like that answer,let see how much you know")
+
+   best_player = input("who is the best soccer player Messi or Cristiano R?" )
+   if answer == "Messi": 
+      print("Alot of people thinks Cristiano ronaldo is better than Messi.!!")
+
+if __name__ == "__main__":
+   main()
--- lab03.py
+++ lab03.py
@@ -12,4 +12,5 @@ def main () :
     conversation ()
     print("thanks for chatting with me")
 
-if__name__ == "__main__"
+if __name__ == "__main__"
+    main()
